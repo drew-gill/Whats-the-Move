d44824f5c61be0f713133a5c0b566d172277b24d
2021fub <78240211+2021fub@users.noreply.github.com>
2021-01-30 17:49:23 -0500

    Added Switches to Menu
    Need to add Icons

--- src/Components/PersistentLeft.js
+++ src/Components/PersistentLeft.js
@@ -18,6 +18,7 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 import LocalDiningIcon from '@material-ui/icons/LocalDining';
+import SwitchesGroup from './SwitchesGroup';
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -130,21 +131,7 @@ export default function PersistentLeft() {
         </div>
         <Divider />
         <List>
-          {['Locally Owned', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <LocalDiningIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
+            <SwitchesGroup/>
         </List>
       </Drawer>
       <main
---
+++ src/Components/SwitchesGroup.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Switch from '@material-ui/core/Switch';
+
+export default function SwitchesGroup() {
+  const [state, setState] = React.useState({
+    gilad: true,
+    jason: false,
+    antoine: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <FormControl component="fieldset">
+      <FormLabel component="legend">Filters</FormLabel>
+      <FormGroup>
+        <FormControlLabel
+          control={<Switch checked={state.local} onChange={handleChange} name="local" />}
+          label="Locally Owned"
+        />
+        <FormControlLabel
+          control={<Switch checked={state.covid} onChange={handleChange} name="covid" />}
+          label="Covid Safe"
+        />
+        <FormControlLabel
+          control={<Switch checked={state.ada} onChange={handleChange} name="ada" />}
+          label="ADA compliant"
+        />
+      </FormGroup>
+      <FormHelperText>YeHaw</FormHelperText>
+    </FormControl>
+  );
+}
